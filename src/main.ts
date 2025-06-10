6a27dee302a5622bbb874cb7f829794c3e40a85c
jamersonsouza <jamersonsouza@outlook.com>
2025-06-09 21:52:52 -0300

    🔧config: criação do arquivo de app config

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { isDevMode, importProvidersFrom } from '@angular/core';
+import {  bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            // Register the ServiceWorker as soon as the application is stable
-            // or after 30 seconds (whichever comes first).
-            registrationStrategy: 'registerWhenStable:30000'
-        })),
-        provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: lara,
-                options: {
-                    darkModeSelector: '.my-app-dark'
-                }
-            },
-            ripple: true
-        })
-    ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
