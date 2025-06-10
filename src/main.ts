c5ae18eab7008bce8f550dced0dfd78c46f6d546
jamersonsouza <jamersonsouza@outlook.com>
2025-06-09 21:47:37 -0300

    🔧config: bootstrap da aplicação utilizando as apis standalone

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { isDevMode, importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: lara,
+                options: {
+                    darkModeSelector: '.my-app-dark'
+                }
+            },
+            ripple: true
+        })
+    ]
+})
   .catch(err => console.error(err));
