62a151363609d14a49cda98b28ae6ddf5e52426d
jamersonsouza <jamersonsouza@outlook.com>
2025-06-12 22:30:07 -0300

    ✨ feat:  adição de rota e finalização da tela de login

--- src/app/modules/pages/login/login.component.ts
+++ src/app/modules/pages/login/login.component.ts
@@ -1,14 +1,23 @@
 import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
-    imports: [ButtonModule],
+    imports: [ButtonModule, InputTextModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
     standalone: true
 })
 export class LoginComponent {
 
+  constructor(private formBuilder : FormBuilder){
+    //to do
+  }
+
+  login(): void{
+    //to do
+  }
 
 }
